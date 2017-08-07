d1d1873ea243c34cb53f663c1197d3a49d705e87
Henry <henry.onyango@outlook.com>
2017-08-07 17:17:45 +0300

    Allowing CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-     origins 'http://localhost:3000/'
+     origins 'http://192.168.42.19/'
 
      resource '*',
        headers: :any,
